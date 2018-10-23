8a49373e76fcdd2a3868f8497d528b1799f77dcc
jb16321 <jb16321@essex.ac.uk>
2018-10-23 16:59:38 +0100

    * INetworkedSensor.cs: Added file * NetworkedSensorDataBundle.cs: Added file

---
+++ IoT_Hub/IoT_Hub/Classes/NetworkedSensorDataBundle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IoT_Hub
+{
+    public class NetworkedSensorDataBundle
+    {
+		List<NetworkedSensorDataItem> dataItems;
+        public NetworkedSensorDataBundle()
+        {
+        }
+
+		public class NetworkedSensorDataItem {
+			
+		}
+
+    }
+}
---
+++ IoT_Hub/IoT_Hub/Interfaces/INetworkedSensor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IoT_Hub
+{
+	/// <summary>
+    /// Interface for a networked sensor to implement
+    /// </summary>
+    public interface INetworkedSensor
+    {
+		NetworkedSensorDataBundle FetchData();
+    }
+}
