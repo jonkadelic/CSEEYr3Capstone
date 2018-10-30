94927d46335779ddbb38d4e0a8c165e40fb62c15
Joe4422 <josephdotbrown@gmail.com>
2018-10-30 14:42:32 +0000

    Added comments

--- DeviceDriver_Lifx_Color_A19/DeviceDriver_Lifx_Color_A19/MainClass.cs
+++ DeviceDriver_Lifx_Color_A19/DeviceDriver_Lifx_Color_A19/MainClass.cs
@@ -11,24 +11,52 @@ namespace DeviceDriver_Lifx_Color_A19
 {
     public class Device : IGenericDevice, ILightBulb_Power, ILightBulb_HSL, ILightBulb_Warmth
     {
-
-        public Device(string Id)
+        /// <summary>
+        ///     Creates a new Device object with unique ID provided by the JSON data from Lifx.
+        /// </summary>
+        /// <param name="Id">
+        ///     The unique Lifx API of the device.
+        /// </param>
+        private Device(string Id)
         {
             this.Id = Id;
         }
 
+        /// <summary>
+        ///     The Lifx API ID of the device.
+        /// </summary>
         private readonly string Id;
 
-        public static string AccessToken => "c9a6ca1e3e7752d0fba45dd24db0202a84cd702839a167c7dd1960a5b111e926";
+        /// <summary>
+        ///     The access token for the Lifx API; needs to be made changeable in the future.
+        /// </summary>
+        private static string AccessToken => "c9a6ca1e3e7752d0fba45dd24db0202a84cd702839a167c7dd1960a5b111e926";
 
-        public static string[] HttpHeader => new[] { "Authorization", "Bearer " + AccessToken };
+        /// <summary>
+        ///     Header for the HTTP request to the Lifx API.
+        /// </summary>
+        private static string[] HttpHeader => new[] { "Authorization", "Bearer " + AccessToken };
 
-        public static string HttpUrl => "https://api.lifx.com/v1/lights/";
+        /// <summary>
+        ///     URL for accessing the Lifx HTTP API.
+        /// </summary>
+        private static string HttpUrl => "https://api.lifx.com/v1/lights/";
 
-        public static string Identifier => "lifx_color_a19";
+        /// <summary>
+        ///     Device identifier for the Lifx JSON.
+        /// </summary>
+        private static string Identifier => "lifx_color_a19";
 
+        /// <summary>
+        ///     List of all devices with Identifier equal to "lifx_color_a19".
+        /// </summary>
+        // TODO: Rewrite as property
         public static List<Device> Devices;
 
+        /// <summary>
+        ///     Label of the device as provided by Lifx JSON.
+        ///     Cannot be changed by public API so invoking set method does nothing.
+        /// </summary>
         public string Label
         {
             get
