f202b718dfcdc3bb9d02ce002f4357b5b5905717
Joe4422 <josephdotbrown@gmail.com>
2018-11-11 13:56:37 +0000

    Turned IDeviceValue into a class.

---
+++ DeviceDriverPluginSystem/DeviceDriverPluginSystem/DeviceValue.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DeviceDriverPluginSystem
+{
+    public class DeviceValue<T> : DeviceValue
+    {
+        public Func<T> Get;
+        public Func<bool, T> Set;
+        public string Label { get; }
+
+        public DeviceValue(Func<T> get, Func<bool, T> set, string label) : base(typeof(T))
+        {
+            Get = get;
+            Set = set;
+            Label = label;
+        }
+    }
+
+    public class DeviceValue
+    {
+        public Type ValueType;
+
+        public DeviceValue(Type type)
+        {
+            ValueType = type;
+        }
+    }
+}
--- DeviceDriverPluginSystem/DeviceDriverPluginSystem/IDeviceValue.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace DeviceDriverPluginSystem
-{
-    public interface IDeviceValue { }
-}
